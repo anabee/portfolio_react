da5a725493f5c7a23d3baf76ca0445319961daa9
Ana Valdivia <anav1317@yahoo.com>
2020-08-21 23:36:58 -0700

    Adding grid to the page

--- src/components/About/title.js
+++ src/components/About/title.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    image: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      maxHeight: 200, 
+    },
+  }));
 
 const Title = () => {
+    const classes = useStyles();
 
     return (
         <div>
-            <img src="/../../border.png" alt="leaves"/>
-            <h1>Ana Valdivia</h1>
-            <img src="/../../Me-min.jpeg" alt="me"/>
+            <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            >
+                <Grid item xs={12}>
+                    <img src="/../../border.png" alt="leaves" className={classes.image}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <h1>Ana Valdivia</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <img src="/../../Me-min.jpeg" alt="me" className={classes.image}/>
+                </Grid>
+            </Grid>
         </div>
     )
 }
